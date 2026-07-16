c5946178184fd37dee7b586cddc9d825c91ee805
hbatz2026 <210033@qq.com>
2026-07-16 09:55:37 +0800

    deploy: bump bundle.js cache bust to v3.0 (hotspot data fix)

--- data/bundle.js
+++ data/bundle.js
@@ -1,5 +1,5 @@
 // 抖本工坊 · 数据包（自动合并 10 文件）
-// 生成时间: 2026-07-16 01:53:08
+// 生成时间: 2026-07-16 01:54:53
 // 合并文件: bgmList.js, dailyScripts.js, hotspotData.js, phonePool.js, publish-kit.js, t1Presets.js, t2Presets.js, t4Presets.js, techDB.js, topicPool.js
 // 大小: 99026 bytes ( 10 source files)
 
